e704b64874111060f5fdb0ccf83cc488cb423588
boomzy <542061862@qq.com>
2020-02-12 11:14:54 +0800

    question update

--- src/leetcode300_easy/Main.java
+++ src/leetcode300_easy/Main.java
@@ -1205,6 +1205,37 @@ public class Main {
         }
         return num;
     }
+
+    /**
+     * LeetCode.263 丑数
+     * 编写一个程序判断给定的数是否为丑数。
+     * <p>
+     * 丑数就是只包含质因数 2, 3, 5 的正整数。
+     *
+     * @param num
+     * @return
+     */
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+                continue;
+            }
+            if (num % 3 == 0) {
+                num /= 3;
+                continue;
+            }
+            if (num % 5 == 0) {
+                num /= 5;
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
 }
 
 class TreeNode {
