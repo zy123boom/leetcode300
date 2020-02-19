8fff46c178e8748ecbd9682364386ca93a745ac4
boomzy <542061862@qq.com>
2020-02-19 11:09:33 +0800

    question update

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -780,6 +780,62 @@ public class Main {
     }
 
     /**
+     * LeetCode.33 搜索旋转排序数组
+     * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+     * <p>
+     * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+     * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+     * 你可以假设数组中不存在重复的元素。
+     * <p>
+     * 你的算法时间复杂度必须是 O(log n) 级别。
+     * <p>
+     * 示例 1:
+     * 输入: nums = [4,5,6,7,0,1,2], target = 0
+     * 输出: 4
+     * <p>
+     * 示例 2:
+     * 输入: nums = [4,5,6,7,0,1,2], target = 3
+     * 输出: -1
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        /*
+            二分法
+         */
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                if (nums[mid] >= nums[0]) {
+                    left = mid + 1;
+                } else {
+                    if (target < nums[0]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+            } else if (nums[mid] > target) {
+                if (nums[mid] >= nums[0]) {
+                    if (target < nums[0]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
      * LeetCode.146 LRU缓存机制
      * <p>
      * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作：
