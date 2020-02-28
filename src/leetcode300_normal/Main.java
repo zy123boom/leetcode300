846154f059ad7451913e1046ec13bae767ee5d5b
boomzy <542061862@qq.com>
2020-02-28 15:52:05 +0800

    question update

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -1399,6 +1399,47 @@ public class Main {
     }
 
     /**
+     * LeetCode.59 螺旋矩阵II
+     * <p>
+     * 给定一个正整数 n，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
+     *
+     * @param n
+     * @return
+     */
+    public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        if (n <= 0) {
+            return res;
+        }
+        int left = 0, right = res.length - 1;
+        int top = 0, bottom = res.length - 1;
+        int k = 1;
+        while (left < right && top < bottom) {
+            for (int i = left; i < right; i++) {
+                res[top][i] = k++;
+            }
+            for (int i = top; i < bottom; i++) {
+                res[i][right] = k++;
+            }
+            for (int i = right; i > left; i--) {
+                res[bottom][i] = k++;
+            }
+            for (int i = bottom; i > top; i--) {
+                res[i][left] = k++;
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        // 如果是奇数矩阵
+        if (n % 2 != 0) {
+            res[n / 2][n / 2] = k;
+        }
+        return res;
+    }
+
+    /**
      * LeetCode.78 子集
      * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）
      *
