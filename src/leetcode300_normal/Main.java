f3296cfe65aca8a36d092896430ea38f24206904
boomzy <542061862@qq.com>
2020-03-17 22:38:53 +0800

    question update

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -2653,6 +2653,66 @@ public class Main {
     }
 
     /**
+     * LeetCode.105 从前序与中序遍历序列构造二叉树
+     * <p>
+     * 根据一棵树的前序遍历与中序遍历构造二叉树。
+     * 注意:
+     * 你可以假设树中没有重复的元素。
+     * <p>
+     * 例如，给出
+     * 前序遍历 preorder = [3,9,20,15,7]
+     * 中序遍历 inorder = [9,3,15,20,7]
+     * 返回如下的二叉树：
+     * <p>
+     *  3
+     * / \
+     * 9  20
+     * /  \
+     * 15   7
+     *
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(preorder, inorder, 0, 0, inorder.length - 1);
+    }
+
+    /**
+     * 105题帮助函数
+     *
+     * @param preorder
+     * @param inorder
+     * @param preStart 先序开始的位置（根节点）
+     * @param inStart 中序开始的位置
+     * @param inEnd 中序结束的位置
+     */
+    private TreeNode helper(int[] preorder, int[] inorder, int preStart, int inStart, int inEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+        // 构造当前根节点
+        int currentVal = preorder[preStart];
+        TreeNode current = new TreeNode(currentVal);
+
+        // 找到根节点在中序遍历中的位置
+        int inIndex = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == currentVal) {
+                inIndex = i;
+            }
+        }
+
+        // 递归的构建左子树和右子树
+        TreeNode left = helper(preorder, inorder, preStart + 1, inStart, inIndex - 1);
+        TreeNode right = helper(preorder, inorder, preStart + inIndex - inStart + 1, inIndex + 1, inEnd);
+
+        current.left = left;
+        current.right = right;
+        return current;
+    }
+
+    /**
      * LeetCode.146 LRU缓存机制
      * <p>
      * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作：
