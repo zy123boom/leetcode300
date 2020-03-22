62adc592cd10943f42033d291c41e8ef2413188b
boomzy <542061862@qq.com>
2020-03-22 12:16:40 +0800

    question update

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -2724,7 +2724,7 @@ public class Main {
      * 后序遍历 postorder = [9,15,7,20,3]
      * 返回如下的二叉树：
      * <p>
-     *  3
+     * 3
      * / \
      * 9  20
      * /  \
@@ -2769,6 +2769,62 @@ public class Main {
 
         current.left = left;
         current.right = right;
+        return current;
+    }
+
+    /**
+     * LeetCode.109 有序链表转换二叉搜索树
+     * <p>
+     * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+     * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+     * <p>
+     * 示例:
+     * 给定的有序链表： [-10, -3, 0, 5, 9]
+     * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+     * <p>
+     *    0
+     *   / \
+     * -3   9
+     * /   /
+     * -10  5
+     *
+     * @param head
+     * @return
+     */
+    public TreeNode sortedListToBST(ListNode head) {
+        /*
+            通过快慢指针找出中间的节点，然后根据中间节点划分成左右两个子问题
+         */
+        if (head == null) {
+            return null;
+        }
+        return helper(head, null);
+    }
+
+    /**
+     * 109题帮助函数
+     *
+     * @param head
+     * @param tail 总问题的结尾
+     * @return
+     */
+    private TreeNode helper(ListNode head, ListNode tail) {
+        if (head == null || head == tail) {
+            return null;
+        }
+
+        // 找中间节点
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != tail && fast.next.next != tail) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        // 根节点
+        TreeNode current = new TreeNode(slow.val);
+        current.left = helper(head, slow);
+        current.right = helper(slow.next, tail);
         return current;
     }
 
