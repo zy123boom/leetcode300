f4a94aabac9d8df939096ba870e4a92339d0185f
boomzy <542061862@qq.com>
2020-03-29 16:30:12 +0800

    question update

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -2837,11 +2837,11 @@ public class Main {
      * 示例:
      * 给定如下二叉树，以及目标和 sum = 22，
      * <p>
-     *       5
-     *      / \
-     *    4    8
-     *   /    / \
-     *  11  13  4
+     * 5
+     * / \
+     * 4    8
+     * /    / \
+     * 11  13  4
      * /  \    / \
      * 7   2  5   1
      * <p>
@@ -2900,24 +2900,24 @@ public class Main {
      * 给定一个二叉树，原地将它展开为链表。
      * <p>
      * 例如，给定二叉树
-     *     1
-     *    / \
-     *   2   5
-     *  / \   \
+     * 1
+     * / \
+     * 2   5
+     * / \   \
      * 3   4   6
      * <p>
      * 将其展开为：
      * 1
-     *  \
-     *   2
-     *    \
-     *     3
-     *      \
-     *       4
-     *        \
-     *         5
-     *          \
-     *           6
+     * \
+     * 2
+     * \
+     * 3
+     * \
+     * 4
+     * \
+     * 5
+     * \
+     * 6
      *
      * @param root
      */
@@ -2937,6 +2937,89 @@ public class Main {
         root.right = prev;
         root.left = null;
         prev = root;
+    }
+
+    /**
+     * LeetCode.116 填充每个节点的下一个右侧节点指针
+     * <p>
+     * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+     * <p>
+     * struct Node {
+     * int val;
+     * Node *left;
+     * Node *right;
+     * Node *next;
+     * }
+     * <p>
+     * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+     * 初始状态下，所有 next 指针都被设置为 NULL。
+     * <p>
+     * 提示：
+     * 你只能使用常量级额外空间。
+     * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度
+     *
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        /*
+            类似层序遍历
+         */
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node current = null;
+            while (size > 0) {
+                Node node = queue.poll();
+                // 注意，先右后左
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                node.next = current;
+                current = node;
+                size--;
+            }
+        }
+        return root;
+        /*
+            解法二：递归
+            if (root == null) {
+                return null;
+            }
+            if (root.left != null) {
+                root.left.next = root.right;
+            }
+            if (root.right != null && root.next != null) {
+                root.right.next = root.next.left;
+            }
+            connect(root.left);
+            connect(root.right);
+            return root;
+         */
+    }
+
+    /**
+     * LeetCode.117 填充每个节点的下一个右侧节点指针 II
+     * <p>
+     * 与I不同的是，完美二叉树变为二叉树。
+     * <p>
+     * 提示：
+     * 树中的节点数小于 6000
+     * -100 <= node.val <= 100
+     *
+     * @param root
+     * @return
+     */
+    public Node connect2(Node root) {
+        // 116题的层次遍历方法依旧可以通过
+        return connect(root);
     }
 
     /**
@@ -3005,6 +3088,27 @@ class ListNode {
 
     public ListNode(int val) {
         this.val = val;
+    }
+}
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
     }
 }
 
