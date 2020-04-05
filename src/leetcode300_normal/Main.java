2dcf0b0d4f27ef893f3aa7dad7b1b9034c137cfd
boomzy <542061862@qq.com>
2020-04-05 17:26:59 +0800

    question update

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -3062,6 +3062,73 @@ public class Main {
     }
 
     /**
+     * LeetCode.129 求根到叶子节点数字之和
+     * <p>
+     * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+     * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+     * 计算从根到叶子节点生成的所有数字之和。
+     * <p>
+     * 说明: 叶子节点是指没有子节点的节点。
+     * <p>
+     * 示例 1:
+     * 输入: [1,2,3]
+     *   1
+     *  / \
+     * 2   3
+     * 输出: 25
+     * 解释:
+     * 从根到叶子节点路径 1->2 代表数字 12.
+     * 从根到叶子节点路径 1->3 代表数字 13.
+     * 因此，数字总和 = 12 + 13 = 25.
+     * <p>
+     * 示例 2:
+     * 输入: [4,9,0,5,1]
+     *     4
+     *    / \
+     *   9   0
+     *  / \
+     * 5   1
+     * 输出: 1026
+     * 解释:
+     * 从根到叶子节点路径 4->9->5 代表数字 495.
+     * 从根到叶子节点路径 4->9->1 代表数字 491.
+     * 从根到叶子节点路径 4->0 代表数字 40.
+     * 因此，数字总和 = 495 + 491 + 40 = 1026.
+     *
+     * @param root
+     * @return
+     */
+    private int sum = 0;
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        helper(root, root.val);
+        return sum;
+    }
+
+    /**
+     * 129题帮助函数
+     *
+     * @param root
+     * @param curSum
+     */
+    private void helper(TreeNode root, int curSum) {
+        if (root.left == null && root.right == null) {
+            sum += curSum;
+            return;
+        }
+
+        if (root.left != null) {
+            helper(root.left, curSum * 10 + root.left.val);
+        }
+
+        if (root.right != null) {
+            helper(root.right, curSum * 10 + root.right.val);
+        }
+    }
+
+    /**
      * LeetCode.146 LRU缓存机制
      * <p>
      * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作：
