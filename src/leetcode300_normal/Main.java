f316729c19c010147fc5f812f71fa54e9c2c18e5
boomzy <542061862@qq.com>
2020-04-08 17:55:28 +0800

    question update

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -3130,6 +3130,78 @@ public class Main {
     }
 
     /**
+     * LeetCode.130 被围绕的区域
+     * <p>
+     * 给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。
+     * 找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+     * <p>
+     * 示例:
+     * X X X X
+     * X O O X
+     * X X O X
+     * X O X X
+     * 运行你的函数后，矩阵变为：
+     * X X X X
+     * X X X X
+     * X X X X
+     * X O X X
+     * <p>
+     * 解释:
+     * 被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，
+     * 或不与边界上的 'O' 相连的 'O' 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称
+     * 它们是“相连”的。
+     *
+     * @param board
+     */
+    private int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public void solve(char[][] board) {
+        // 先填充外侧，剩下的就是内侧了
+        if (board == null || board.length == 0) {
+            return;
+        }
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            dfs(board, 0, i);
+            dfs(board, m - 1, i);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    /**
+     * 130题帮助函数
+     *
+     * @param board
+     * @param row
+     * @param column
+     */
+    private void dfs(char[][] board, int row, int column) {
+        if (row < 0 || row >= board.length || column < 0 || column >= board[0].length
+                || board[row][column] != 'O') {
+            return;
+        }
+        board[row][column] = 'T';
+        for (int[] d : direction) {
+            dfs(board, row + d[0], column + d[1]);
+        }
+    }
+
+    /**
      * LeetCode.131 分割回文串
      * <p>
      * 给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
