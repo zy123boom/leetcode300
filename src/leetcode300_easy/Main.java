8c0bc77592c61c3dc6aecf9dbaf6bbeb0b0aeb4e
boomzy <542061862@qq.com>
2020-04-09 21:57:39 +0800

    question update

--- src/leetcode300_easy/Main.java
+++ src/leetcode300_easy/Main.java
@@ -794,6 +794,33 @@ public class Main {
     }
 
     /**
+     * LeetCode.136 只出现一次的数字
+     * <p>
+     * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+     * <p>
+     * 说明：
+     * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+     * <p>
+     * 示例 1:
+     * 输入: [2,2,1]
+     * 输出: 1
+     * <p>
+     * 示例 2:
+     * 输入: [4,1,2,1,2]
+     * 输出: 4
+     *
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+
+    /**
      * LeetCode.141 环形链表
      * 判断链表是否有环
      *
