e65422fbb7dd75ebee4df26dfa75724b79f267fa
boomzy <542061862@qq.com>
2020-04-15 19:35:20 +0800

    question update

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -4025,6 +4025,49 @@ public class Main {
         }
         return max;
     }
+
+    /**
+     * LeetCode.162 寻找峰值
+     * <p>
+     * 峰值元素是指其值大于左右相邻值的元素。
+     * 给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+     * 数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+     * 你可以假设 nums[-1] = nums[n] = -∞。
+     * <p>
+     * 示例 1:
+     * 输入: nums = [1,2,3,1]
+     * 输出: 2
+     * 解释: 3 是峰值元素，你的函数应该返回其索引 2。
+     * <p>
+     * 示例 2:
+     * 输入: nums = [1,2,1,3,5,6,4]
+     * 输出: 1 或 5
+     * 解释: 你的函数可以返回索引 1，其峰值元素为 2；
+     *      或者返回索引 5， 其峰值元素为 6。
+     * <p>
+     * 说明:
+     * 你的解法应该是 O(logN) 时间复杂度的。
+     *
+     * @param nums
+     * @return
+     */
+    public int findPeakElement(int[] nums) {
+        /*
+            根据题目要求，使用二分。
+            如果nums[mid] < nums[mid + 1]，此时还是上升的趋势，答案肯定在后一半。
+            反之，已经是下降的趋势，答案已经出现过了，所以在前半段。
+         */
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
 }
 
 class TreeNode {
