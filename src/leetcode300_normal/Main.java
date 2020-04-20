45c3108a4a5228841df3dd423d039ade53dff240
boomzy <542061862@qq.com>
2020-04-20 10:04:13 +0800

    question update

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -4239,6 +4239,63 @@ public class Main {
         }
         return res;
     }
+
+    /**
+     * LeetCode.200 岛屿数量
+     * <p>
+     * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+     * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+     * 此外，你可以假设该网格的四条边均被水包围。
+     * <p>
+     * 示例 1:
+     * 输入:
+     * 11110
+     * 11010
+     * 11000
+     * 00000
+     * 输出: 1
+     * <p>
+     * 示例 2:
+     * 输入:
+     * 11000
+     * 11000
+     * 00100
+     * 00011
+     * 输出: 3
+     * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+     *
+     * @param grid
+     * @return
+     */
+    int[][] directionForNumIslands = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int numIslands(char[][] grid) {
+        /*
+            DFS+递归，模板题目
+         */
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != '0') {
+                    numIslandsDFS(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void numIslandsDFS(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') {
+            return;
+        }
+        grid[row][col] = '0';
+        for (int[] d : directionForNumIslands) {
+            numIslandsDFS(grid, row + d[0], col + d[1]);
+        }
+    }
 }
 
 class TreeNode {
