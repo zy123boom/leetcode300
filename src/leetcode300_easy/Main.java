3a39beae599b69e0893c26f30678cb867f3f8be2
boomzy <542061862@qq.com>
2022-05-14 17:23:41 +0800

    commit

--- src/leetcode300_easy/Main.java
+++ src/leetcode300_easy/Main.java
@@ -1056,6 +1056,18 @@ public class Main {
             notRob = pre;
         }
         return Math.max(rob, notRob);
+        /*
+            if (nums.length < 2) {
+            return nums.length == 0 ? 0 : nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = nums[0] > nums[1] ? nums[0] : nums[1];
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+         */
     }
 
     /**
