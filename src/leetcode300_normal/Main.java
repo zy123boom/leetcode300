08c54ccff8541d243c3142d9730e0c7ab3db7b96
boomzy <542061862@qq.com>
2022-05-14 11:01:14 +0800

    commit

--- src/leetcode300_normal/Main.java
+++ src/leetcode300_normal/Main.java
@@ -4127,6 +4127,81 @@ public class Main {
     }
 
     /**
+     * leetcode.166 分数到小数
+     * <p>
+     * 给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以 字符串形式返回小数 。
+     * 如果小数部分为循环小数，则将循环的部分括在括号内。
+     * 如果存在多个答案，只需返回 任意一个 。
+     * 对于所有给定的输入，保证 答案字符串的长度小于 104 。
+     * <p>
+     * 示例 1：
+     * 输入：numerator = 1, denominator = 2
+     * 输出："0.5"
+     * <p>
+     * 示例 2：
+     * 输入：numerator = 2, denominator = 1
+     * 输出："2"
+     * <p>
+     * 示例 3：
+     * 输入：numerator = 4, denominator = 333
+     * 输出："0.(012)"
+     *
+     * @param numerator
+     * @param denominator
+     * @return
+     */
+    public String fractionToDecimal(int numerator, int denominator) {
+        /*
+            小数点后的循环体寻找标志，若一个数第二次出现，则前面的出现过的小数就是循环体
+         */
+        if (denominator == 0 || numerator == 0) {
+            return "0";
+        }
+
+        StringBuilder res = new StringBuilder();
+        // 获取符号位的技巧
+        String sign = (numerator > 0) ^ (denominator > 0) ? "-" : "";
+        res.append(sign);
+
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
+
+        res.append(num / den);
+        num %= den;
+
+        // num %= den 为0说明能够整除，直接返回结果
+        if (num == 0) {
+            return res.toString();
+        }
+
+        // 开始小数部分，设置小数点
+        res.append(".");
+
+        // key: 每次计算得到的商，value: 对应所在位置
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(num, res.length());
+
+        // 没有除尽，就循环处理
+        while (num != 0) {
+            num *= 10;
+            res.append(num / den);
+            num %= den;
+
+            // 若一个数第二次出现，则前面的出现过的小数就是循环体
+            if (map.containsKey(num)) {
+                int index = map.get(num);
+                res.insert(index, "(");
+                res.append(")");
+                break;
+            } else {
+                map.put(num, res.length());
+            }
+        }
+
+        return res.toString();
+    }
+
+    /**
      * LeetCode.179 最大数
      * <p>
      * 给定一组非负整数，重新排列它们的顺序使之组成一个最大的整数。
